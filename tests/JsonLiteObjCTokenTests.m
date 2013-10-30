c5eef2cef6e167c964ba79299810b9f9f8685ec7
andrii-mamchur <amam@softserveinc.com>
2013-10-30 20:26:46 +0200

    Changed C language dialect to compiler default

--- tests/JsonLiteObjCTokenTests.m
+++ tests/JsonLiteObjCTokenTests.m
@@ -18,6 +18,8 @@
 #import "JsonLiteAccumulator.h"
 
 #include "jsonlite.h"
+#import "JsonLiteSerializer.h"
+#import "JsonLiteAccumulator.h"
 
 @implementation JsonLiteObjCTokenTests
 
@@ -461,6 +463,55 @@
     jsonlite_result result = jsonlite_parser_tokenize(ps, buffer, size);
     STAssertTrue(result == jsonlite_result_ok, @"Bad error code");
     jsonlite_parser_release(ps);
+}
+
+- (void)testNumber {
+    NSNumber *n1 = [NSNumber numberWithLongLong:4278190080];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1, n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
+}
+
+- (void)testMaxLong {
+    NSNumber *n1 = [NSNumber numberWithLong:LONG_MAX];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
+}
+
+- (void)testMinLong {
+    NSNumber *n1 = [NSNumber numberWithLong:LONG_MIN];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
+}
+
+- (void)testMaxLongP1 {
+    NSNumber *n1 = [NSNumber numberWithLongLong:(long long)LONG_MAX + 1ll];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
+}
+
+- (void)testMinLongM1 {
+    NSNumber *n1 = [NSNumber numberWithLongLong:LONG_MIN - 1ll];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
+}
+
+- (void)testMaxLongLong {
+    NSNumber *n1 = [NSNumber numberWithLongLong:LONG_LONG_MAX];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
+}
+
+- (void)testMinLongLong {
+    NSNumber *n1 = [NSNumber numberWithLongLong:LONG_LONG_MIN];
+    NSData *data  = [[JsonLiteSerializer serializer] serializeObject:@[n1]];
+    NSNumber *n2 = [[JsonLiteAccumulator objectFromData:data withMaxDepth:16] lastObject];
+    STAssertEqualObjects(n1, n2, @"Object not equals");
 }
 
 @end
